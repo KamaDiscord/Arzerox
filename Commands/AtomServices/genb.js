2996691d000a5af399697842ae94acc708b2eb2d
Kama <lordkama_temp@yopmail.com>
2022-06-05 20:06:48 +0200

    test simple gen

--- Commands/AtomServices/genb.js
+++ Commands/AtomServices/genb.js
@@ -1,8 +1,13 @@
 module.exports = {
-    name:"gen b",
+    name:"gens",
     description:"gen item from a list",
     execute(client, message, args, Discord) {
         const splitargs = message.content.trim().split(' ');
         const arg1 = splitargs[0];
+        if(arg1 === "a"){
+            message.channel.send("a")
+        } else if(arg1 === "b"){
+            message.channel.send("b")
+        }
     }
 }
