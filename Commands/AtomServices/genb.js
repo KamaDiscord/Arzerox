791c3c2f6de90370d3a9d33badafbce11b6a0525
Kama <lordkama_temp@yopmail.com>
2022-06-05 20:15:22 +0200

    log

--- Commands/AtomServices/genb.js
+++ Commands/AtomServices/genb.js
@@ -4,7 +4,7 @@ module.exports = {
     execute(client, message, args, Discord) {
         const splitargs = message.content.trim().split(' ');
         console.log(splitargs)
-        const arg1 = splitargs[0];
+        const arg1 = splitargs[1];
         if(arg1 === "a"){
             console.log("a")
         } else if(arg1 === "b"){
