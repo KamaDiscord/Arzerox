28dc74604711dd3a0877eb4b94e44d3ecd3b507d
Kama <lordkama_temp@yopmail.com>
2022-06-06 15:30:18 +0200

    test

--- Commands/AtomServices/genb.js
+++ Commands/AtomServices/genb.js
@@ -2,9 +2,9 @@ module.exports = {
     name:"gens",
     description:"gen item from a list",
     execute(client, message, args, Discord) {
-        args = global.args
-        console.log(message.content + args)
-        const arg1 = args[1];
+        const splitargs = message.content.trim().split(' ');
+        console.log(args[0])
+        const arg1 = splitargs[1];
         if(arg1 === "a"){
             console.log("a")
         } else if(arg1 === "b"){
