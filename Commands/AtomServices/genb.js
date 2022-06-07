24cf34b0ec36ab7765d9e416cab7d5fd70832baf
Kama <lordkama_temp@yopmail.com>
2022-06-07 11:59:02 +0200

    test args last?

--- Commands/AtomServices/genb.js
+++ Commands/AtomServices/genb.js
@@ -1,6 +1,9 @@
 module.exports = {
     name:"gens",
     description:"gen item from a list",
+        /**
+     * @param {Array[]} args Arguments splitted by spaces after the command name
+     */
     execute(client, message, args, Discord) {
         const splitargs = message.content.trim().split(' ');
         console.log(args[0])
