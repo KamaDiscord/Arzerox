c767f2cdf690a1a9fd95a1a0d37e3f28c905797d
Kama <82283685+LordKamaYT@users.noreply.github.com>
2022-06-27 13:15:18 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
+const { Client, Intents } = require('discord.js');
 const Discord = require('discord.js');
 require('dotenv').config();
-const client = new Discord.Client();
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 const { Collection } = require('discord.js')
 const mongoose = require('mongoose');
 const CatLoggr = require('cat-loggr');
