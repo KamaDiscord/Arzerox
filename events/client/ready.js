86e7943930323bcd169d0c933c9283f722586cd7
Kama <82283685+LordKamaYT@users.noreply.github.com>
2022-10-10 14:37:33 +0200

    Update ready.js

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,13 +1,13 @@
 const client = require("../../main.js");
 const User = require("../../models/User");
 
-client.on("ready", () => {
-  console.log(`${client.user.username} Is Online`);
-  client.user.setActivity({
-    name: `+help ● Dev by Kama#4725`,
-    type: "WATCHING",
-    url:"https://twitch.tv/lordkama_fr",
-  });
+//client.on("ready", () => {
+//  console.log(`${client.user.username} Is Online`);
+//  client.user.setActivity({
+//    name: `+help ● Dev by Kama#4725`,
+//    type: "WATCHING",
+//    url:"https://twitch.tv/lordkama_fr",
+//  });
 
   // code
   const users = await User.find();
