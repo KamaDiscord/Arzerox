7be6225e81049512edd3ff533227b32127eedad0
Kama <82283685+LordKamaYT@users.noreply.github.com>
2022-10-10 14:28:07 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { client, GatewayIntentBits } = require('discord.js');
+const { Client, GatewayIntentBits } = require('discord.js');
 const Discord = require('discord.js');
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 require('dotenv').config();
