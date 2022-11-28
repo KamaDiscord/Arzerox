ca123efe926da4f89299e8cc02d3926494981bee
Kama <82283685+LordKamaYT@users.noreply.github.com>
2022-11-28 11:10:49 +0100

    Update event_handler.js

--- handlers/event_handler.js
+++ handlers/event_handler.js
@@ -7,7 +7,7 @@ module.exports = (Discord, client) => {
         for(const file of event_files){
             const event = require(`../events/${dirs}/${file}`);
             const event_name = file.split('.')[0];
-            if (typeof event?.bind === 'function');
+            if (typeof event?.bind === 'function')
             client.on(event_name, event.bind(null, Discord, client))
         }
     }
