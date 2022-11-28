90b33cd3fbd7cfd81c6d17d752b9ee3a95f4dfc6
Kama <82283685+LordKamaYT@users.noreply.github.com>
2022-11-28 11:36:41 +0100

    Update message.js

--- events/guild/message.js
+++ events/guild/message.js
@@ -1,32 +1,30 @@
 require('dotenv').config();
 const Discord = require('discord.js')
-module.exports = (Discord, client, message) =>{
-    init = async =>{
-        const prefix = process.env.PREFIX;
-        if(!message.content.startsWith(prefix) || message.author.bot) return;
+module.exports = (Discord, client, message) async =>{
+    const prefix = process.env.PREFIX;
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
 
-        const args = message.content.slice(prefix.length).trim().split(/  +/g);
-        const command = args.shift().toLowerCase();
-        const cmd = client.commands.get(command, interaction.commandName);
+    const args = message.content.slice(prefix.length).trim().split(/  +/g);
+    const command = args.shift().toLowerCase();
+    const cmd = client.commands.get(command, interaction.commandName);
 
-        const User = require("../Models/User");
-        if (cmd) {
-        let user = client.userSettings.get(interaction.user.id);
-        // If there is no user, create it in the Database as "newUser"
-        if (!user) {
-          const findUser = await User.findOne({ Id: interaction.user.id });
-          if (!findUser) {
-            const newUser = await User.create({ Id: interaction.user.id });
-            client.userSettings.set(interaction.user.id, newUser);
-            user = newUser;
-          } else return;
-        }
-        if (cmd.premium && user && !user.isPremium) {
-          interaction.followUp(`You are not premium user`);
-        } else {
-          cmd.execute(client, message, args, Discord);
-        }
-        }
+    const User = require("../Models/User");
+    if (cmd) {
+    let user = client.userSettings.get(interaction.user.id);
+    // If there is no user, create it in the Database as "newUser"
+    if (!user) {
+      const findUser = await User.findOne({ Id: interaction.user.id });
+      if (!findUser) {
+        const newUser = await User.create({ Id: interaction.user.id });
+        client.userSettings.set(interaction.user.id, newUser);
+        user = newUser;
+      } else return;
     }
-    init{};
+    if (cmd.premium && user && !user.isPremium) {
+      interaction.followUp(`You are not premium user`);
+    } else {
+      cmd.execute(client, message, args, Discord);
+    }
+    }
+
 }
