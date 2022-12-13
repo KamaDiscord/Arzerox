f73a0217a685f3d982646ff5b18850825c72069d
Kama <82283685+LordKamaYT@users.noreply.github.com>
2022-12-13 17:02:10 +0100

    Update event_handler.js

--- handlers/event_handler.js
+++ handlers/event_handler.js
@@ -3,12 +3,14 @@ const fs = require('fs');
 module.exports = (client, Discord) => {
     const load_dir = (dirs) =>{
         const event_files = fs.readdirSync(`./events/${dirs}`).filter(file => file.endsWith('.js'));
+        console.log(client);
 
         for(const file of event_files){
             const event = require(`../events/${dirs}/${file}`);
             const event_name = file.split('.')[0];
             if (typeof event?.bind === 'function')
             client.on(event_name, event.bind(null, Discord, client))
+            console.log(client);
         }
     }
 
